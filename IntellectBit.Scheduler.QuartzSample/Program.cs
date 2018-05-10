070504d662f53d07696f2b71f4007e9b4ca8f43b
Saran <saran.rk@gmail.com>
2018-05-10 18:08:39 +0530

    Initial fixes

--- IntellectBit.Scheduler.QuartzSample/Program.cs
+++ IntellectBit.Scheduler.QuartzSample/Program.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using Common.Logging;
+using Quartz;
+using Quartz.Impl;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace IntellectBit.Scheduler.QuartzSample
@@ -10,6 +14,35 @@ namespace IntellectBit.Scheduler.QuartzSample
     {
         static void Main(string[] args)
         {
+            ILog log = LogManager.GetLogger(typeof(Program));
+
+            // First we must get a reference to a scheduler
+            ISchedulerFactory sf = new StdSchedulerFactory();
+            IScheduler sched = sf.GetScheduler().Result;
+
+            try
+            {
+                var startTime = DateTimeOffset.Now.AddSeconds(5);
+
+                var job = JobBuilder.Create<SimpleJob>()
+                                    .WithIdentity("job1", "group1")
+                                    .Build();
+
+                var trigger = TriggerBuilder.Create()
+                    .WithIdentity("trigger1", "group1")
+                    .StartAt(startTime)
+                    .WithSimpleSchedule(x => x.WithIntervalInSeconds(10).RepeatForever())
+                    .Build();
+
+                sched.ScheduleJob(job, trigger);
+
+                sched.Start();
+                Thread.Sleep(TimeSpan.FromMinutes(2));
+            }
+            finally
+            {
+                sched.Shutdown(true);
+            }
         }
     }
 }
---
+++ IntellectBit.Scheduler.QuartzSample/SimpleJob.cs
@@ -0,0 +1,14 @@
+﻿using Common.Logging;
+using Quartz;
+using System.Threading.Tasks;
+
+public class SimpleJob : IJob
+{
+    private static ILog logging = LogManager.GetLogger(typeof(SimpleJob));
+
+    public Task Execute(IJobExecutionContext context)
+    {
+        logging.InfoFormat("Hello from job");
+        return null;
+    }
+}
